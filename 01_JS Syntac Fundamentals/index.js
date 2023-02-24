35b0ec54e453fcb41fd4bb3286af7cae2000e19d
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-02-24 23:45:30 +0200

    Add new tasks from first lesson

--- 01_JS Syntac Fundamentals/index.js
+++ 01_JS Syntac Fundamentals/index.js
@@ -1,4 +1,7 @@
-for (let i = 0; i <= 10; i++) {
-    const element = array[i];
+// for (let i = 0; i <= 10; i++) {
+//     const element = array[i];
     
-}
+// }
+
+let firstName = 'Pesho';
+console.log(firstName.toUpperCase());
