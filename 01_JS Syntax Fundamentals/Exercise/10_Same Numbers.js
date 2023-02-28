a5771260987acd1bc1da40942cb6d0b086cfe863
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-02-28 22:22:54 +0200

    Add new files

--- 01_JS Syntax Fundamentals/Exercise/10_Same Numbers.js
+++ 01_JS Syntax Fundamentals/Exercise/10_Same Numbers.js
@@ -1,23 +1,44 @@
+// FIRST OPTION - !Not all the tests passed!
+
+// function sameNumbers(number) {
+//     let numberToString = number.toString();
+//     let isFalse = false;
+//     let sum = 0;
+
+//     for (let i = 0; i < numberToString.length; i++) {
+//         if (i !== (numberToString.length - 1) && numberToString[i] === numberToString[i+1]) {
+//             isFalse = true
+//         } else if (i === (numberToString.length - 1) && numberToString[i] === numberToString[i-1]) {
+//             isFalse = true
+//         } else {
+//             isFalse = false
+//             break;
+//         }
+//     }
+//     for (const num of numberToString) {
+//         sum += Number(num)
+//     }
+//     console.log(isFalse)
+//     console.log(sum)
+// }
+
+// SECOND OPTION
+
 function sameNumbers(number) {
     let numberToString = number.toString();
-    let isFalse = false;
-    let sum = 0;
+    let numberArr = numberToString.split('');
+    // Are all the digits equal:
+    console.log(numberArr.every(val => val === numberArr[0]));
 
-    for (let i = 0; i < numberToString.length; i++) {
-        if (i !== (numberToString.length - 1) && numberToString[i] === numberToString[i+1]) {
-            isFalse = true
-        } else if (i === (numberToString.length - 1) && numberToString[i] === numberToString[i-1]) {
-            isFalse = true
-        } else {
-            isFalse = false
-            break;
-        }
+    // Sum all the digits in the number
+    let sum = 0;
+    while (number) {
+        sum += number % 10;
+        number = Math.floor(number / 10);
     }
-    for (const num of numberToString) {
-        sum += Number(num)
-    }
-    console.log(isFalse)
     console.log(sum)
 }
+
+
 sameNumbers(2222222)
 sameNumbers(12322)
--- 01_JS Syntax Fundamentals/Exercise/19_String Substring.js
+++ 01_JS Syntax Fundamentals/Exercise/19_String Substring.js
@@ -1,17 +1,31 @@
+// FIRST VARIATION
+
+// function solve(word, text) {
+//     let textArr = text.split(' ');
+//     let isFound = false;
+//     for (const w of textArr) {
+//         if (word === w.toLowerCase()) {
+//             console.log(word);
+//             isFound = true;
+//             break;
+//         }
+//     }
+//     if (isFound === false) {
+//         console.log(`${word} not found!`)
+//     }
+// }
+
+// SECOND VARIATION
+
 function solve(word, text) {
-    let textArr = text.split(' ')
-    let isFound = false
+    let textArr = text.split(' ');
     for (const w of textArr) {
         if (word === w.toLowerCase()) {
-            console.log(word);
-            isFound = true;
-            break;
+            return word;
         }
     }
-    if (isFound === false) {
-        console.log(`${word} not found!`)
-    }
+    return `${word} not found!`;
 }
 
-solve('javascript','JavaScript is the best programming language')
-solve('python','JavaScript is the best programming language')
+console.log(solve('javascript','JavaScript is the best programming language'))
+console.log(solve('python','JavaScript is the best programming language'))
