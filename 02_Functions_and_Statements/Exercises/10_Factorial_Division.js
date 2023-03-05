aa5bd6081ee6a1153646e4d8c57c38e85e6199d0
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-03-05 15:21:25 +0200

    Rename main folders

--- 02_Functions_and_Statements/Exercises/10_Factorial_Division.js
+++ 02_Functions_and_Statements/Exercises/10_Factorial_Division.js
@@ -1,8 +1,8 @@
 function factorialDivision(firstNum, secondNum) {
 
     function factorial(number) {
-        if (number === 0) {
-            return 1;
+        if (number === 1) {
+            return number;
         }
         return number * factorial(number - 1);
     }
