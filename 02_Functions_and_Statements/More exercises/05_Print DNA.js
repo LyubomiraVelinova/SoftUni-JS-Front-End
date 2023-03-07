c3a45f392a3d5c3861d0a7f4f6d3c4cc0e7c1200
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-03-07 18:55:01 +0200

    Rename gitignore file

--- 02_Functions_and_Statements/More exercises/05_Print DNA.js
+++ 02_Functions_and_Statements/More exercises/05_Print DNA.js
@@ -1,13 +1,39 @@
-function printDNA(number) {
-    let stringDNA = '*'.repeat(6);
-    let sequence = 'ATCGTTAGGG';
-    for (let i = 0; i < number; i++) {
-        for (let j = 0; j < sequence.length; j += 2) {
-            stringDNA[2] = sequence[j]
+// function printDNA(number) {
+//     let stringDNA = '*'.repeat(6);
+//     let sequence = 'ATCGTTAGGG';
+//     for (let i = 0; i < number; i++) {
+    
+//         let currentLetters = sequence.substring(0, 2);
+//         sequence = sequence.substring(2) + currentLetters;
+
+//         console.log(currentLetters);
+//         console.log(sequence);
             
-        }
+//     }
+//     console.log(stringDNA)
+// }
+
+function printDNA(number) {
+    let stringDNA = ('*'.repeat(6)).split('');
+    let sequence = 'ATCGTTAGGG'.split('');
+    for (let i = 0; i < number; i++) {
+    
+        let currentLetterLeft = sequence.shift();
+        sequence.push(currentLetterLeft);
+        let currentLetterRight = sequence.shift();
+        sequence.push(currentLetterRight);
+        let n = 0;
+        for (let j = 2; j >= 0; j--) {
+            stringDNA[2 - n] = currentLetterLeft;
+            stringDNA[3 + n] = currentLetterRight;
+            
+            if (n === 2) {
+                n = 0;
+            }
+            n += 1
+            console.log(stringDNA.join(''));
+        } 
     }
-    console.log(stringDNA)
 }
 
 printDNA(4);
