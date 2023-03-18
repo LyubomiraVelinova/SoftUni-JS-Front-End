eb8d51a5916677295e5b322bfde6124e93739ddf
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-03-18 15:21:24 +0200

    Add new tasks

--- 04_DOM_and_Events/Lab/03. Collect-List-Items/CollectListItems.js
+++ 04_DOM_and_Events/Lab/03. Collect-List-Items/CollectListItems.js
@@ -6,11 +6,3 @@ function extractText() {
             textArea.textContent += li.textContent + '\n';
         })
 }
-
-// function extractText() {
-//     const liItems = Array.from(document.querySelectorAll( ul > li));
-//     const textArea = document.getElementById('result');
-//     for (const item of liItems) {
-//         textArea.textContent += item.textContent + '\n';
-//     }
-// }
--- 04_DOM_and_Events/Lab/05. Delete-From-Table/app.js
+++ 04_DOM_and_Events/Lab/05. Delete-From-Table/app.js
@@ -11,3 +11,17 @@ function deleteByEmail() {
         result.textContent = 'Not found';
     }
 }
+
+function deleteByEmail() {
+    const emailsInput = Array.from(document.querySelectorAll('td:nth-child(even)'));
+    const input = document.querySelector('body > label > input[type=text]');
+    const result = document.querySelector('#result')
+    for (const email of emailsInput) {
+        if (email.textContent === input.value){
+            email.parentElement.remove();
+            result.textContent = 'Deleted.';
+        } else {
+            result.textContent = 'Not found';
+        }
+    }
+}
