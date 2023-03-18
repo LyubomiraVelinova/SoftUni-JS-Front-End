421fa4b2d0b34f7db84a7fa8370e859508f3e102
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-03-18 23:32:55 +0200

    Add new tasks from DOM_and_EVENTS

--- 04_DOM_and_Events/Exercise/02. Sections/script.js
+++ 04_DOM_and_Events/Exercise/02. Sections/script.js
@@ -1,3 +1,14 @@
 function create(words) {
-   console.log('TODO:...');
+   const div = document.getElementById('content');
+   words
+      .forEach(word => {
+         let newDiv = document.createElement('div');
+         let p = document.createElement('p');
+         p.textContent = word;
+         p.style.display = 'none';
+         div.appendChild(newDiv).appendChild(p);
+         newDiv.addEventListener('click', function(e){
+            p.style.display = 'block'
+         })
+      });
 }
