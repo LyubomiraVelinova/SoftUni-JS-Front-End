aeeb8b2a203fde19f655ecd31aae055dbca41de1
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-03-19 00:06:52 +0200

    Add new files

--- 04_DOM_and_Events/Exercise/03. Accordion/accordion.js
+++ 04_DOM_and_Events/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,16 @@
 function toggle() {
-    console.log('TODO:...');
+    const text = document.getElementById('extra');
+    const button = Array.from(document.getElementsByClassName('button'));
+    let count = 0;
+
+    button[0].onclick = function() {
+        count ++ ;
+        if (count % 2 == 1) {
+            text.style.display = 'block';
+            button[0].textContent = 'Less';
+        } else {
+            text.style.display = 'none';
+            button[0].textContent = 'More';
+        }
+    }   
 }
