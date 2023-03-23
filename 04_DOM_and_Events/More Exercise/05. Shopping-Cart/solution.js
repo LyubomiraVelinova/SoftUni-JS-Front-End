59f6eb1d983d3c44235fa24c26420a1dd31d20f2
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-03-24 00:44:28 +0200

    Add new files

--- 04_DOM_and_Events/More Exercise/05. Shopping-Cart/solution.js
+++ 04_DOM_and_Events/More Exercise/05. Shopping-Cart/solution.js
@@ -1,3 +1,32 @@
 function solve() {
-   //TODO...
+   const addBtn = Array.from(document.querySelectorAll('.add-product'));
+   const textArea = document.querySelector(`body > div > textarea`);
+   const checkoutBtn = document.querySelector(`body > div > button`);
+   addBtn.forEach(button => {
+      button.addEventListener('click', productsAppearance);
+   });
+   checkoutBtn.addEventListener('click', calculateTotal);
+
+
+   let total = 0;
+   let listOfProducts = [];
+   function productsAppearance(e) {
+      const currentProduct = e.target.parentNode.parentNode;
+      const name = (currentProduct.children[1].children[0]).textContent;
+      listOfProducts.push(name);
+      const money = Number(currentProduct.children[3].textContent);
+      total += money;
+      console.log(total)
+      textArea.value += `Added ${name} for ${money.toFixed(2)} to the cart.\n`;
+   }
+
+   function calculateTotal() {
+      listOfProducts = [... new Set(listOfProducts)];
+      textArea.value += `You bought ${listOfProducts.join(', ')} for ${total.toFixed(2)}.`
+
+      addBtn.forEach(btn => {
+         btn.disabled = true;
+      });
+      checkoutBtn.disabled = true;
+   }
 }
--- 04_DOM_and_Events/More Exercise/06. Pascal-or-Camel-Case/solution.js
+++ 04_DOM_and_Events/More Exercise/06. Pascal-or-Camel-Case/solution.js
@@ -1,3 +1,79 @@
 function solve() {
-  //TODO...
+  const input = document.getElementById('text');
+  const [example, requiredType] = input.value.split(', ');
+  const exampleArr = example.split(' ');
+  const requiredTypeArr = requiredType.split(' ');
+
+  const namingConvention = document.getElementById(`naming-convention`);
+
+  let result = '';
+  exampleArr.forEach(word => {
+    word = word.toLowerCase();
+    if (requiredType === "Camel Case") {
+      if (exampleArr.indexOf(word) === 0) {
+        word = word.slice(1, word.length);
+        result += word;
+      } else if (exampleArr.indexOf(word) === exampleArr.length - 1) {
+        word = word.slice(0, word.length - 1);
+        result += capitalizeFirstLetter(word);
+      } else {
+        result += capitalizeFirstLetter(word);
+      }
+    } else if (requiredType === 'Pascal Case') {
+      result += capitalizeFirstLetter(word);
+    } else {
+      result = 'Error!'
+    }
+  });
+  namingConvention.value = result;
+
+  function capitalizeFirstLetter (string) {
+    let stringWithoutFirstLetter = string.slice(1);
+    let stringFirstLetter = string.charAt(0);
+    string = stringFirstLetter.toUpperCase() + stringWithoutFirstLetter;
+    return string
+  }
 }
+
+
+
+
+
+
+// function solve() {
+//   // const input = document.getElementById('text');
+//   // const [example, requiredType] = input.value.split(', ');
+//   const exampleArr = ['"this', 'is', 'an', 'example"'];
+//   const requiredType = "Camel Case";
+//   // const namingConvention = document.getElementById(`naming-convention`);
+
+//   let result = '';
+//   exampleArr.forEach(word => {
+//     word = word.toLowerCase();
+//     if (requiredType === "Camel Case") {
+//       if (exampleArr.indexOf(word) === 0) {
+//         word = word.slice(1, word.length);
+//         result += word;
+//       } else if (exampleArr.indexOf(word) === exampleArr.length - 1) {
+//         word = word.slice(0, word.length - 1);
+//         result += capitalizeFirstLetter(word);
+//       } else {
+//         result += capitalizeFirstLetter(word);
+//       }
+//     } else if (requiredType === 'Pascal Case') {
+//       result += capitalizeFirstLetter(word);
+//     } else {
+//       result = 'Error!'
+//     }
+//   });
+//   namingConvention.value = result;
+
+//   function capitalizeFirstLetter (string) {
+//     let stringWithoutFirstLetter = string.slice(1);
+//     let stringFirstLetter = string.charAt(0);
+//     string = stringFirstLetter.toUpperCase() + stringWithoutFirstLetter;
+//     return string
+//   }
+// }
+
+// solve()
