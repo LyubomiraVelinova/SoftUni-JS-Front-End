1b07755280763716d1fff27fcc8c5320585aab96
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-03-27 23:12:27 +0300

    Add new tasks HTTP and REST

--- 05_HTTP_and_REST/Exercise/01.Bus-Stop/app.js
+++ 05_HTTP_and_REST/Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,24 @@
 function getInfo() {
-    // TODO:
+    const BASE_URL = 'http://localhost:3030/jsonstore/bus/businfo/'
+    const stopIdInput = document.getElementById('stopId');
+    const busStopResult = document.getElementById('stopName');
+    const busesInput = document.getElementById('buses');
+
+    busesInput.innerHTML = '';
+    fetch(BASE_URL + stopIdInput.value)
+        .then((res) => res.json())
+        .then((busInfo) => {
+            const{name, buses} = busInfo;
+            busStopResult.textContent = name;
+            for (const busId in buses) {
+                const li = document.createElement('li');
+                li.textContent = `Bus ${busId} arrives in ${buses[busId]} minutes`;
+                busesInput.appendChild(li)
+            }
+        })
+        .catch((err) => {
+            busStopResult.textContent = 'Error'
+        })
+    
+
 }
