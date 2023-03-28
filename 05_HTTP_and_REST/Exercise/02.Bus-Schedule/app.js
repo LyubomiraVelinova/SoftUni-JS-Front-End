9832fbe300e54431ccc28632811a7937a4473e7d
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-03-28 15:28:29 +0300

    Add new files

--- 05_HTTP_and_REST/Exercise/02.Bus-Schedule/app.js
+++ 05_HTTP_and_REST/Exercise/02.Bus-Schedule/app.js
@@ -1,10 +1,32 @@
 function solve() {
+    const BASE_URL = 'http://localhost:3030/jsonstore/bus/schedule/'
+    const info = document.getElementById('info').children[0];
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+
+    let nextBusId = 'depot';
+    let currentBusStopName = '';
     function depart() {
-        // TODO: 
+        fetch(`${BASE_URL}${nextBusId}`)
+            .then((res) => res.json())
+            .then((data) => {
+                currentBusStopName = data['name'];
+                nextBusId = data['next'];
+                info.textContent = `Next stop ${currentBusStopName}`;
+                departBtn.disabled = true;
+                arriveBtn.disabled = false;
+            })
+            .catch((err) => {
+                info.textContent = `Error`;
+                departBtn.disabled = true;
+                arriveBtn.disabled = true;
+            });
     }
 
     async function arrive() {
-        // TODO:
+        info.textContent = `Arriving at ${currentBusStopName}`;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
