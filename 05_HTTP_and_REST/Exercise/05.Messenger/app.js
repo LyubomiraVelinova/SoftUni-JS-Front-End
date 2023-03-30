516249101f878f023198ddd937018b1f1bad8a36
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-03-30 15:02:18 +0300

    Add new files

--- 05_HTTP_and_REST/Exercise/05.Messenger/app.js
+++ 05_HTTP_and_REST/Exercise/05.Messenger/app.js
@@ -1,5 +1,44 @@
 function attachEvents() {
-    // TODO:
+    const BASE_URL = 'http://localhost:3030/jsonstore/messenger';
+    const sendBtn = document.getElementById('submit');
+    const refreshBtn = document.getElementById('refresh');
+    const textArea = document.getElementById('messages');
+    const nameInput = document.querySelector('#controls > div:nth-child(1) > input[type=text]');
+    const messageInput = document.querySelector('#controls > div:nth-child(2) > input[type=text]');
+
+    sendBtn.addEventListener('click', sendInfo);
+    refreshBtn.addEventListener('click', refreshInfo);
+
+    function sendInfo() {
+        let authorName = nameInput.value;
+        let msgText = messageInput.value;
+
+        fetch(BASE_URL, {
+            method: 'post',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                author: authorName,
+                content: msgText,
+            })
+        })
+    }
+
+    function refreshInfo() {
+        textArea.textContent = '';
+        let result = [];
+        fetch(BASE_URL).then((res) => res.json()).then((data) => {
+            for (const key in data) {
+                let author = data[key].author;
+                let message = data[key].content;
+                result.push(`${author}: ${message}`)
+            }
+            textArea.value = result.join('\n');
+        })
+        nameInput.value = '';
+        messageInput.value = '';
+    }
 }
 
 attachEvents();
--- 05_HTTP_and_REST/Exercise/06.Phonebook/app.js
+++ 05_HTTP_and_REST/Exercise/06.Phonebook/app.js
@@ -1,5 +1,64 @@
 function attachEvents() {
-    // TODO:
+    const BASE_URL = 'http://localhost:3030/jsonstore/phonebook';
+    const btnLoad = document.getElementById('btnLoad');
+    const createBtn = document.getElementById('btnCreate');
+    const personInput = document.getElementById('person');
+    const phoneInput = document.getElementById('phone');
+    const phoneBookContainer = document.getElementById('phonebook');
+
+    btnLoad.addEventListener('click', loadPhoneBookHandler);
+    createBtn.addEventListener('click', createContact);
+
+    async function loadPhoneBookHandler() {
+        try {
+            const phoneBookRes = await fetch(BASE_URL);
+            const phoneBookEntries = await phoneBookRes.json();
+            let phoneBookData = Object.values(phoneBookEntries);
+            phoneBookContainer.innerHTML = '';
+            for (const { phone, person, _id } of phoneBookData) {
+                const li = document.createElement('li');
+                li.innerHTML = `${person}: ${phone}`;
+                const button = document.createElement('button');
+                button.innerText = 'Delete';
+                button.id = _id;
+                button.addEventListener('click', deletePhoneBookHandler);
+                li.appendChild(button)
+                phoneBookContainer.appendChild(li);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    function createContact() {
+        const person = personInput.value;
+        const phone = phoneInput.value;
+
+        fetch(BASE_URL, {
+            method: 'POST',
+            headers: { 'Content-type': 'application\json' },
+            body: JSON.stringify({ person, phone })
+        })
+            .then((res) => res.json())
+            .then(() => {
+                loadPhoneBookHandler();
+                personInput.value = '';
+                phoneInput.value = '';
+            })
+            .catch((err) => console.log(err))
+    }
+
+    async function deletePhoneBookHandler() {
+        const id = this.id;
+        // OR const id = e.currentTarget.id;
+        const httpHeaders = {
+            method: 'DELETE'
+        };
+        fetch(`${BASE_URL}/${id}`, httpHeaders)
+            .then((res) => res.json())
+            .then(loadPhoneBookHandler)
+            .catch((err) => console.log(err));
+    }
 }
 
 attachEvents();
