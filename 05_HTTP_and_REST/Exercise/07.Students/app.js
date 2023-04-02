4a3054a1f16bbdbea256b95b88bf8d16e1dbe97b
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-04-02 21:44:11 +0300

    Add new files

--- 05_HTTP_and_REST/Exercise/07.Students/app.js
+++ 05_HTTP_and_REST/Exercise/07.Students/app.js
@@ -2,36 +2,60 @@ function attachEvents() {
   const BASE_URL = 'http://localhost:3030/jsonstore/collections/students';
   const inputs = document.querySelectorAll('#form > div.inputs')[0];
   const [firstNameInput, lastNameInput, facultyNumInput, gradeInput] = inputs.children;
+  const tbody = document.querySelector('#results > tbody');
   const submitBtn = document.getElementById('submit');
   submitBtn.addEventListener('click', createNewStudent);
 
-  listStudents()
+  function checkCorrectInputs() {
+    for (const item of inputs) {
+      if (!item.value.trim()) {
+        return false
+      }
+    }
+    return true
+  }
+  listStudents();
 
   function listStudents() {
+    tbody.innerHTML = ''
     fetch(BASE_URL)
       .then((res) => res.json())
       .then((data) => {
         let students = Object.values(data);
         students.forEach(student => {
-          console.log(student)
           const tr = document.createElement('tr');
           tr.innerHTML = `
-          <td>${student.firstName}</td>
-          <td>${student.lastName}</td>
-          <td>${student.facultyNumber}</td>
-          <td>${student.grade}</td>
-          `;
+            <td>${student.firstName}</td>
+            <td>${student.lastName}</td>
+            <td>${student.facultyNumber}</td>
+            <td>${student.grade}</td>
+            `;
+          tbody.appendChild(tr);
         });
       })
   }
 
   function createNewStudent() {
-    console.log(firstNameInput.value)
-    console.log(lastNameInput.value)
-    console.log(facultyNumInput.value)
-    console.log(gradeInput.value)
+    if (!checkCorrectInputs()) {
+      return
+    }
+
+    fetch(BASE_URL, {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({
+        firstName: firstNameInput.value,
+        lastName: lastNameInput.value,
+        facultyNumber: facultyNumInput.value,
+        grade: gradeInput.value,
+      }),
+    })
+    listStudents();
+    firstNameInput.value = null;
+    lastNameInput.value = null;
+    facultyNumInput.value = null;
+    gradeInput.value = null;
   }
 }
-
 
 attachEvents();
--- 05_HTTP_and_REST/Exercise/08.Book-library/app.js
+++ 05_HTTP_and_REST/Exercise/08.Book-library/app.js
@@ -1,5 +1,58 @@
 function attachEvents() {
-  // TODO:
-}
+  const BASE_URL = 'http://localhost:3030/jsonstore/collections/books/';
+  const booksContainer = document.querySelector('body > table > tbody');
+  // const tbody = booksContainer.parentNode;
+  const loadBtn = document.getElementById('loadBooks');
+  const submitBtn = document.querySelector('#form button');
+  const inputs = Array.from(document.querySelectorAll('#form input'));
 
-attachEvents();
+  loadBtn.addEventListener('click', loadingBooks);
+  submitBtn.addEventListener('click', createBook);
+
+  function loadingBooks() {
+    booksContainer.innerHTML = '';
+    fetch(BASE_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        for (let { author, title } of Object.values(data)) {
+          booksContainer.innerHTML = `
+          <td>${title}</td>
+          <td>${author}</td>
+          <td>
+              <button>Edit</button>
+              <button>Delete</button>
+          </td>
+        `
+        }
+      })
+      .catch((err) => console.error(err))
+  }
+
+  function createBook() {
+    if (inputs[0].value !== '' && inputs[1].value !== '') {
+      let newTitle = inputs[0];
+      let newAuthor = inputs[1];
+      fetch((BASE_URL), {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({
+          author: newAuthor,
+          title: newTitle
+          
+          const newTr = document.createElement('tr');
+          newTr.innerHTML = `
+            <td>${newTitle}</td>
+            <td>${newAuthor}</td>
+            <td>
+                <button>Edit</button>
+                <button>Delete</button>
+            </td>
+          `
+          tbody.appendChild(newTr);
+        })
+      })
+
+    }
+  }
+
+  attachEvents();
